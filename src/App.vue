35cc2e954104229d4073c8728e9adf3cd71e33d4
e1bandito <aleksandr.shitov@mail.ru>
2021-02-11 15:44:34 +0300

    add counter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Counter
+      :count="count"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import Counter from '@/components/Counter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Counter,
+  },
+  data() {
+    return {
+      count: 0,
+    };
   },
 };
 </script>
@@ -21,8 +28,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="counter">
+    Очков: {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: {
+    count: Number,
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
