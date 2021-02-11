71328b5adda7ad8d016ac4568323f5c3eb83bd10
e1bandito <aleksandr.shitov@mail.ru>
2021-02-11 15:52:05 +0300

    add SelectNumber

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,60 @@
     <Counter
       :count="count"
     />
+    <SelectNumber
+      :numbers="numbers"
+    />
   </div>
 </template>
 
 <script>
 
 import Counter from '@/components/Counter.vue';
+import SelectNumber from '@/components/SelectNumber.vue';
 
 export default {
   name: 'App',
   components: {
+    SelectNumber,
     Counter,
   },
   data() {
     return {
       count: 0,
+      numbers: [
+        {
+          value: 2,
+          selected: false,
+        },
+        {
+          value: 3,
+          selected: false,
+        },
+        {
+          value: 4,
+          selected: false,
+        },
+        {
+          value: 5,
+          selected: false,
+        },
+        {
+          value: 6,
+          selected: false,
+        },
+        {
+          value: 7,
+          selected: false,
+        },
+        {
+          value: 8,
+          selected: false,
+        },
+        {
+          value: 9,
+          selected: false,
+        },
+      ],
     };
   },
 };
---
+++ src/components/SelectNumber.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="select-number">
+    <h2 class="select-number__title">Выбери цифру, для тренировки</h2>
+    <ul class="select-number__list">
+      <li class="select-number__item"
+        v-for="(item, index) in numbers"
+        :key="index"
+      >{{ item.value }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectNumber',
+  props: {
+    numbers: Array,
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
