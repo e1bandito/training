0d149630851a78755ae79fca0c91f46dbb18010a
e1bandito <aleksandr.shitov@mail.ru>
2021-02-13 14:06:01 +0300

    add styles for Result

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -63,6 +63,85 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss">
+@import "src/assets/styles/variables";
+@import "src/assets/styles/mixins";
+.result {
+  padding: 30px 0;
 
+  & .counter {
+    margin-bottom: 30px;
+  }
+
+  @include max(500) {
+    padding: 10px 0;
+  }
+}
+.result__inner {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.result__text {
+  color: $grey;
+  margin: 0 0 30px 0;
+  font-size: 80px;
+  text-align: center;
+  font-weight: 700;
+  @include max(500) {
+    font-size: 40px;
+    margin-bottom: 10px;
+  }
+  &--success {
+    color: $green;
+  }
+  &--error {
+    color: $red;
+  }
+}
+.result__exp-text {
+  text-align: center;
+  color: $grey;
+  margin: 0;
+  font-size: 30px;
+  font-weight: 700;
+  @include max(500) {
+    font-size: 20px;
+  }
+}
+.result__exp-val {
+  color: $red;
+  text-align: center;
+  margin: 0 0 30px 0;
+  font-size: 50px;
+  font-weight: 700;
+  @include max(500) {
+    margin-bottom: 10px;
+    font-size: 36px;
+  }
+}
+.result__counter-text {
+  text-align: center;
+  margin: 0;
+  font-size: 30px;
+  color: $grey;
+  font-weight: 700;
+}
+.result__counter-val {
+  text-align: center;
+  color: $grey;
+  margin: 0 0 20px 0;
+  font-size: 140px;
+  font-weight: 700;
+  @include max(500) {
+    margin-bottom: 20px;
+    font-size: 50px;
+  }
+  &--sub {
+    color: $red;
+  }
+  &--up {
+    color: $green;
+  }
+}
 </style>
